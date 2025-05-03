aa7b2fb10f2c7e9d15ab74272c21e373a1866879
pain406553 <pain406553@gmail.com>
2025-05-03 17:13:19 +0300

    Add files via upload

---
+++ js_page.js
@@ -0,0 +1,9 @@
+function ChangeBackgroundToRed(){
+    document.body.style.backgroundColor='black';
+};
+function ChangeBackgroundToBlue(){
+    document.body.style.backgroundColor='white';
+};
+function show_more(Type_Devieses){
+    alert(" More Details about "+ Type_Devieses)
+}
